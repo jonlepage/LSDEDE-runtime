f4a39a3f16535b7074ad485bb1e7e508c2e32566
djmisterjon <jon.lepage@live.ca>
2026-04-19 17:08:41 -0400

    feat: telemetric

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,7 +1,23 @@
+import posthog from "posthog-js";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./App.css";
+
+posthog.init("phc_ytWJv3iJS9BKoXoFAac8P7MGGfUppdxxojecAXsr8aFY", {
+  api_host: "https://e.lepasoft.com",
+  ui_host: "https://us.posthog.com",
+  autocapture: true,
+  capture_pageview: true,
+  capture_pageleave: true,
+  session_recording: {
+    maskAllInputs: false,
+    maskTextSelector: "",
+    recordCrossOriginIframes: false,
+    canvasFps: 4,
+    canvasQuality: 0.4,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
